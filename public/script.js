4d7f1e850c96c04c6dd5643fa6839a252f097955
안동대 백수 <79616817+sasileunnadojalmorem@users.noreply.github.com>
2024-05-05 23:49:50 +0900

    11

--- public/script.js
+++ public/script.js
@@ -170,7 +170,7 @@ function loadLabeledImages() {
     labels.map(async label => {
       const descriptions = []
       for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/sasileunnadojalmorem/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
+        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/sasileunnadojalmorem/Face-Recognition-JavaScript/master/public/labeled_images/${label}/${i}.jpg`)
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
         descriptions.push(detections.descriptor)
       }
